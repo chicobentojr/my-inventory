28e4224430809687c8af646eb8121bd4fd660bed
Francisco Bento <franciscobdsjunior@gmail.com>
2022-10-24 02:25:01 -0300

    Remove unused files

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from "../views/HomeView.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name: "home", component: HomeView, },
-    { path: "/about", name: "about", component: () => import("../views/AboutView.vue"), },
+    { path: "/", name: "home", component: () => import("@/pages/BranchesPage.vue"), },
     { path: "/branches", name: "branches", component: () => import("@/pages/BranchesPage.vue"), },
     { path: "/computers", name: "computers", component: () => import("@/pages/ComputersPage.vue"), },
     { path: "/air-conditioners", name: "air-conditioners", component: () => import("@/pages/AirConditionersPage.vue"), },
